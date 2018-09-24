36d4c504cc3b711b5694b7215cac33b919a6e24a
muge <muge.hasilci@hotmail.com>
2018-09-24 11:32:39 +0100

    Final Test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,5 +2,6 @@
 def fizzbuzz(number)
   return 'fizzbuzz' if (number % 5).zero? && (number % 3).zero?
   return 'buzz' if (number % 5).zero?
-  'fizz'
+  return 'fizz' if (number % 3).zero?
+  number
 end
